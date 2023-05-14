c953093017ca35e7c2f427abc4be01e32f72ebcd
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-05-14 19:28:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -36,13 +36,13 @@ class Animal {
   }
 
   makeSound() {
-    console.log("The " + this._species + " makes a sound.");
+    console.log("The " + this._species + " makes a sound");
   }
 }
 
 class Cat extends Animal {
   purr() {
-    console.log("The " + this._species + " makes a sound.");
+    console.log("The " + this._species + " makes a sound");
   }
 }
 
