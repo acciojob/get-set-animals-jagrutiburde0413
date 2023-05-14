59b1c98bc56d39d059a5461d3890668bee5ad493
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-05-14 19:22:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,72 @@
-//complete this code
+// //complete this code
+// class Animal {
+// 	constructor(species){
+// 		this._species=species
+// 	}
+// 		get species(){
+// 			return this._species
+// 		}
+// 		 makeSound() {
+//     console.log("The " + this._species + " makes a sound.");
+//   }
+	
+// }
+
+// class Dog extends Animal {
+// 	bark(){
+// 		console.log("woof")
+// 	}
+// }
+
+// class Cat extends Animal {
+// 	purr(){
+// 		console.log("purr")
+// 	}
+// }
+// const animal1=new Animal("Siamese")
+
+// const animal2=new Animal("Retriever")
 class Animal {
-	constructor(species){
-		this._species=species
-	}
-		get species(){
-			return this._species
-		}
-		 makeSound() {
+  constructor(species) {
+    this._species = species;
+  }
+
+  get species() {
+    return this._species;
+  }
+
+  makeSound() {
     console.log("The " + this._species + " makes a sound.");
   }
-	
-}
-
-class Dog extends Animal {
-	bark(){
-		console.log("woof")
-	}
 }
 
 class Cat extends Animal {
-	purr(){
-		console.log("purr")
-	}
+  purr() {
+    console.log("The cat purrs.");
+  }
 }
-const animal1=new Animal("Siamese")
 
-const animal2=new Animal("Retriever")
+class Dog extends Animal {
+  bark() {
+    console.log("The dog barks.");
+  }
+}
+
+
+const animal = new Animal("unknown");
+console.log(animal.species); // "unknown"
+animal.makeSound(); // "The unknown makes a sound."
+
+const cat = new Cat("cat");
+console.log(cat.species); // "cat"
+cat.makeSound(); // "The cat makes a sound."
+cat.purr(); // "The cat purrs."
+
+const dog = new Dog("dog");
+console.log(dog.species); // "dog"
+dog.makeSound(); // "The dog makes a sound."
+dog.bark(); // "The dog barks."
+
 //Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
